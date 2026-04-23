fd59203dfc7598b72eb4d934a9588425e100a9da
Pryzmm <pryzmmtheproto@gmail.com>
2026-04-23 16:09:37 -0500

    Fixed some goofy 1.20.1 neoforge stuff

--- common/src/main/java/com/pryzmm/coreconfig/ui/objects/CCElement.java
+++ common/src/main/java/com/pryzmm/coreconfig/ui/objects/CCElement.java
@@ -12,4 +12,10 @@ public interface CCElement {
     int getHeight();
     @Nullable Integer getColor();
     void visitWidgets(Consumer<AbstractWidget> consumer);
+
+    // Neoforge 1.20.1 (for whatever reason) has a very hard time with getting the proper methods for this class, so remapped methods are here as a fallback.
+    default int m_252754_() { return getX(); }
+    default int m_252907_() { return getY(); }
+    default int m_5711_()   { return getWidth(); }
+    default int m_93694_()  { return getHeight(); }
 }
